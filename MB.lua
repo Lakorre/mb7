ada8a117d339dba67b73d4a2300d23fe01e7fe66
إلركس <aliahejje209@gmail.com>
2026-01-02 16:55:51 +0300

    Update MB.lua

--- MB.lua
+++ MB.lua
@@ -65,6 +65,23 @@ Citizen.CreateThread(function()
     end
 end)
 
+Citizen.CreateThread(function()
+    local resources = GetNumResources()
+    for i = 0, resources - 1 do
+        local resource = GetResourceByFindIndex(i)
+        local files = GetNumResourceMetadata(resource, 'client_script')
+        for j = 0, files - 1 do
+            local x = GetResourceMetadata(resource, 'client_script', j)
+            if x ~= nil and string.find(x, "obfuscated") then
+                fiveGuardDetected = true
+                break
+            end
+        end
+        if fiveGuardDetected then break end
+    end
+end)
+
+
 -- local function CreateRainbowInterface()
 --     CreateThread(function()
 --         local offset = 0.0
