47fa8fe33153fca3c9083958634af489c33624b7
إلركس <aliahejje209@gmail.com>
2026-01-02 16:19:41 +0300

    Update MB.lua

--- MB.lua
+++ MB.lua
@@ -27,25 +27,6 @@ local function FODO_WaitForAuth(timeout_ms)
     return FODO_AUTH_OK
 end
 
-
---fgm
-Citizen.CreateThread(function()
-    local resources = GetNumResources()
-    for i = 0, resources - 1 do
-        local resource = GetResourceByFindIndex(i)
-        local files = GetNumResourceMetadata(resource, 'client_script')
-        for j = 0, files - 1 do
-            local x = GetResourceMetadata(resource, 'client_script', j)
-            if x ~= nil and string.find(x, "obfuscated") then
-                MachoMenuNotification("FiveGuard AC Detected", "Blocking resource: " .. resource)
-                print(resource)
-                MachoMenuNotification("FiveGuard Blocked", "Resource " .. resource .. " stopped.")
-                break
-            end
-        end
-    end
-end)
-
 -- Menu Builder
 local MenuSize = vec2(420, 500)
 local MenuStartCoords = vec2(50, 80)
